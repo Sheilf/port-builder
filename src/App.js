b8e126294f95022bea84aed0affd377fdc3d8123
Sheilf <sheilf.code@gmail.com>
2020-12-31 14:16:52 -0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { ChartBuilder } from './ChartBuilder/ChartBuilder'
+
 function App() {
 
   //app setup. Tunnel content through here
   return (
     <main>
       Port Builder App. 
+      <ChartBuilder />
+
+
     </main>
   );
 }
---
+++ src/ChartBuilder/ChartBuilder.js
@@ -0,0 +1,15 @@
+import { Button } from 'react-bootstrap'
+
+export const ChartBuilder = () => {
+
+  //app setup. Tunnel content through here
+  return (
+    <section>
+        ChartBuilder
+
+      
+        <Button>x</Button>
+    </section>
+  );
+}
+
